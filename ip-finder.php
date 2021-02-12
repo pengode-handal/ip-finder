e843cd000b3e3a9cad9384883f8fc23cfb2fefc2
BabwaXgura <77978775+BabwaXgura@users.noreply.github.com>
2021-02-12 10:38:24 +0700

    Create ip-finder.php

---
+++ ip-finder.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="description" content="Make U Eazily To Exploiting">
+      
+    <!-- Open Graph Meta-->
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="ip-finder - MCA Tools">
+    <meta property="og:title" content="ip-finder - MCA">
+    
+    <script>
+      function MainTance(){
+        swal("Coming Soon...", "Kami akan segera kembali", "info")
+      }
+    </script>
+    <title>ip-finder - MCA Tools</title>
+<!-- * ip-finder v1.0.1
+     * Author by: ./BabwaXgura
+     * Code by:  /BabwaXgura
+     * github: https://github.com/BabwaXgura-->
+     
+    <!-- NO BUG BOUNTY PROGRAM -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" href="https://e.top4top.io/p_18508kitp0.jpg" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,XMRstylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="https://aafexploiterteam.my.id/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://aafexploiterteam.my.id/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="https://aafexploiterteam.my.id/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="https://aafexploiterteam.my.id/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="https://aafexploiterteam.my.id/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="https://aafexploiterteam.my.id/assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="https://aafexploiterteam.my.id/assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="https://aafexploiterteam.my.id/assets/css/style.css" rel="stylesheet">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <!--Floating WhatsApp css-->
+  <link rel="stylesheet" href="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/floating-wpp.min.css">
+  <!--Floating WhatsApp javascript-->
+  <script type="text/javascript" src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/floating-wpp.min.js">
+  </script>
+  </head>
+<!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+  <div class="container d-flex align-items-center">
+
+  <div class="logo mr-auto">
+  <h1 class="text-light"><a href="index.html"><span>MCA || OFFICIAL</span></a></h1>
+  
+    </div>
+
+  <nav class="nav-menu d-none d-lg-block">
+  <ul>
+  <li class="active"><a href="index.php">Home</a></li>
+  <li><a href="#about">About</a></li>
+  <ul class="list-unstyled components mb-5">
+<li>
+<a href="http://muslimcyberanonymous.freevar.com/tools" target="_blank">Tools</a></li>
+  <li><a href="https://wibu-elit.blogspot.com/?m=1" target="_blank">WIBU ELITE</a></li>
+  <li><a href="#contact">Contact</a></li>
+
+  <li class="get-started"><a href="https://shn.nasihosting.com/">Bergabung Sekarang</a></li>
+  </ul>
+  </nav><!-- .nav-menu -->
+
+  </div>
+  </header><!-- End Header -->
+  <font color="red">
+<main class="app-content">
+	<div class="app-title">
+		<div>
+          <h1><i class="fa fa-cog"></i> Defacer Tools</h1>
+        </div>
+        <ul class="app-breadcrumb breadcrumb">
+          <a href="https://muslimcyberanonymous.nasiwebhost.com">home
+          	<li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></a></li>
+          
+          <li class="breadcrumb-item">Ip finder</li>
+        </ul>
+      </div>
+	</div>
+	
+<body>
+<br>
+<br>
+<center>
+<iframe src="https://ipinfo.info/ipchecker/index.php" style="border-color:green" width=500 height=700 frameborder=5 ></iframe>
+</center>
+<br><br>
+</body>
+	  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="row d-flex align-items-center">
+        <div class="col-lg-6 text-lg-left text-center">
+          <div class="copyright">
+            &copy; Copyright <strong>BabwaXgura</strong>. All Rights Reserved
+          </div>
+          <div class="credits">
+            Designed by <a href="http://muslimcyberanonymous.freevar.com">./BabwaXgura</a>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <nav class="footer-links text-lg-right text-center pt-2 pt-lg-0">
+            <a href="#intro" class="scrollto">Home</a>
+            <a href="#about" class="scrollto">About</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms of Use</a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- End Footer -->
+ <!-- Vendor JS Files -->
+ <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+ <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+ <script src="https://aafexploiterteam.my.id/assets/vendor/jquery/jquery.min.js"></script> 
+ <script src="https://aafexploiterteam.my.id/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+ <script src="https://aafexploiterteam.my.id/assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+ <script src="https://aafexploiterteam.my.id/assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+ <script src="https://aafexploiterteam.my.id/assets/vendor/php-email-form/validate.js"></script>
+ <script src="https://aafexploiterteam.my.id/assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+ <script src="https://aafexploiterteam.my.id/assets/vendor/counterup/counterup.min.js"></script>
+ <script src="https://aafexploiterteam.my.id/assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+ <script src="https://aafexploiterteam.my.id/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+ <script src="https://aafexploiterteam.my.id/assets/vendor/venobox/venobox.min.js"></script>
+ <script src="https://aafexploiterteam.my.id/assets/vendor/aos/aos.js"></script>
+ <!-- Template Main JS File -->
+ <script src="https://aafexploiterteam.my.id/assets/js/main.js"></script>
+ </body> 
+<script src="https://tools.xploitsecid.or.id/js/jquery-3.2.1.min.js"></script>
+    <script src="https://tools.xploitsecid.or.id/js/popper.min.js"></script>
+    <script src="https://tools.xploitsecid.or.id/js/bootstrap.min.js"></script>
+    <script src="https://tools.xploitsecid.or.id/js/main.js"></script>
+    <!-- The javascript plugin to display page loading on top-->
+    <script src="https://tools.xploitsecid.or.id/js/plugins/pace.min.js"></script>
+    <!-- Page specific javascripts-->
+    <script type="text/javascript" src="https://tools.xploitsecid.or.id/js/plugins/chart.js"></script>
+    <script type="text/javascript">
+      var data = {
+      	labels: ["January", "February", "March", "April", "May"],
+      	datasets: [
+      		{
+      			label: "My First dataset",
+      			fillColor: "rgba(220,220,220,0.2)",
+      			strokeColor: "rgba(220,220,220,1)",
+      			pointColor: "rgba(220,220,220,1)",
+      			pointStrokeColor: "#fff",
+      			pointHighlightFill: "#fff",
+      			pointHighlightStroke: "rgba(220,220,220,1)",
+      			data: [65, 59, 80, 81, 56]
+      		},
+      		{
+      			label: "My Second dataset",
+      			fillColor: "rgba(151,187,205,0.2)",
+      			strokeColor: "rgba(151,187,205,1)",
+      			pointColor: "rgba(151,187,205,1)",
+      			pointStrokeColor: "#fff",
+      			pointHighlightFill: "#fff",
+      			pointHighlightStroke: "rgba(151,187,205,1)",
+      			data: [28, 48, 40, 19, 86]
+      		}
+      	]
+      };
+      var pdata = [
+      	{
+      		value: 300,
+      		color: "#46BFBD",
+      		highlight: "#5AD3D1",
+      		label: "Complete"
+      	},
+      	{
+      		value: 50,
+      		color:"#F7464A",
+      		highlight: "#FF5A5E",
+      		label: "In-Progress"
+      	}
+      ]
+      
+      var ctxl = $("#lineChartDemo").get(0).getContext("2d");
+      var lineChart = new Chart(ctxl).Line(data);
+      
+      var ctxp = $("#pieChartDemo").get(0).getContext("2d");
+      var pieChart = new Chart(ctxp).Pie(pdata);
+    </script>
+    <!-- Google analytics script-->
+    <script type="text/javascript">
+      if(document.location.hostname == 'pratikborsadiya.in') {
+      	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      	ga('create', 'UA-72504830-1', 'auto');
+      	ga('send', 'pageview');
+      }
+    </script>
+  </body>
+</html>
